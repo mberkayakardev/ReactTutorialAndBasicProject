6f12eeb09da00ad8f5542945cfac0675826cb5bf
mberkayakardev <mberkayakar@outlook.com>
2024-03-03 17:23:30 +0300

    first lesson

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main>
+        <div className='container'>
+          <h1> Bu bir deneme yazısısıdr .</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
